d5578374069517c6308bb70da00b58dcdb913a34
adrianmcg85 <adrian.m.mcg@gmail.com>
2019-11-02 14:48:30 +0000

    add header tag to master

--- master.php
+++ master.php
@@ -8,7 +8,9 @@
     <link rel="stylesheet" href="node_modules\bootstrap\dist\css\bootstrap.min.css">
 </head>
 <body>
+<header>
 
+</header>
     <script src="node_modules\bootstrap\dist\js\bootstrap.min.js"></script>
 </body>
 </html>
