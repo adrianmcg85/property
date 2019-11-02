6e35c0ad7e1b13723202e65daf0644af1339d8ce
adrianmcg85 <adrian.m.mcg@gmail.com>
2019-11-02 23:13:19 +0000

    navigation menu basic setup

--- master.php
+++ master.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,13 +9,15 @@
     <link rel="stylesheet" href="node_modules\bootstrap\dist\css\bootstrap.min.css">
     <link rel="stylesheet" href="src/core/style.min.css">
 </head>
-<body>
-<header>
-<?php include'strip/navigation.php' ?>
 
-</header>
-    <script src="node_modules\bootstrap\dist\js\bootstrap.min.js"></script>
+<body>
+    <header>
+        <?php include'strip/navigation.php' ?>
+
+    </header>
     <script src="node_modules\jquery\dist\jquery.min.js"></script>
+    <script src="node_modules\bootstrap\dist\js\bootstrap.min.js"></script>
     <script src="src\core\script.min.js"></script>
 </body>
+
 </html>
---
+++ strip/navigation.php
@@ -0,0 +1,12 @@
+<nav class="navbar navbar-expand-lg navbar-light">
+    <a href="#" class="navbar-brand">Brand</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#targetNav">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="targetNav">
+        <li class="nav-item active">
+            <a class="nav-link" href="#">Home</a>
+        </li>
+        </ul>
+    </div>
+</nav>
