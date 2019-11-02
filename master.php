170546689b6ef7be6502bfb337e351d88184cccc
adrianmcg85 <adrian.m.mcg@gmail.com>
2019-11-02 13:54:59 +0000

    added bootstrap css and js to master

--- master.php
+++ master.php
@@ -5,8 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Master Estate Agent</title>
+    <link rel="stylesheet" href="node_modules\bootstrap\dist\css\bootstrap.min.css">
 </head>
 <body>
-    
+
+    <script src="node_modules\bootstrap\dist\js\bootstrap.min.js"></script>
 </body>
 </html>
