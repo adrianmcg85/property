48394fc3feaa314f52d74b3d47c19fa36c172e80
adrianmcg85 <adrian.m.mcg@gmail.com>
2019-11-02 22:47:41 +0000

    added duplicate site index url

---
+++ index_jnp.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>JNP</title>
+    <link rel="stylesheet" href="node_modules\bootstrap\dist\css\bootstrap.min.css">
+    <link rel="stylesheet" href="src/core/style.min.css">
+</head>
+<body>
+<header>
+<?php include'strip/navigation.php' ?>
+
+</header>
+    <script src="node_modules\bootstrap\dist\js\bootstrap.min.js"></script>
+    <script src="node_modules\jquery\dist\jquery.min.js"></script>
+    <script src="src\core\script.min.js"></script>
+</body>
+</html>
