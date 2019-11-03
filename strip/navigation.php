f4c75798ecb6fa6c5276eb5f3f0dc3ad050da629
adrianmcg85 <adrian.m.mcg@gmail.com>
2019-11-03 20:22:32 +0000

    nav positioning

--- strip/navigation.php
+++ strip/navigation.php
@@ -1,27 +1,50 @@
-<nav class="navbar navbar-expand-lg navbar-light">
-    <a href="#" class="navbar-brand">Brand</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#targetNav">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="targetNav">
-        <li class="nav-item active">
-            <a class="nav-link" href="#">Buy</a>
-        </li>
-        <li class="nav-item active">
-            <a class="nav-link" href="#">Sell</a>
-        </li>
-        <li class="nav-item active">
-            <a class="nav-link" href="#">Rent</a>
-        </li>
-        <li class="nav-item active">
-            <a class="nav-link" href="#">Landlord</a>
-        </li>
-        <li class="nav-item active">
-            <a class="nav-link" href="#">Services</a>
-        </li>
-        <li class="nav-item active">
-            <a class="nav-link" href="#">Branches</a>
-        </li>
-        </ul>
+<div class="container">
+<div class="row">
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="logo">
+            <a href="#" class="navbar-brand">Brand</a>
+        </div>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#targetNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="targetNav">
+            <ul>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Buy</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Sell</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Rent</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Landlord</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Services</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Branches</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <form>
+        <fieldset>
+            <div class="row">
+                <input type="text" name="Smart Search" id="">
+            </div>
+            <div class="form_submit">
+                <button class="btn">S</button>
+            </div>
+        </fieldset>
+    </form>
+    <div class="account">
+        <span class="account-name">MyMove</span>
+        <br>
+        <a class="account-link">Login</a>
+        <a class="account-link">Register</a>
     </div>
-</nav>
+</div>
+</div>
