fd199f54857723d00297eda94246f6d1c7883e57
adrianmcg85 <adrian.m.mcg@gmail.com>
2019-11-03 15:46:47 +0000

    switched stylesheet order

--- index_jnp.php
+++ index_jnp.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>JNP</title>
-    <link rel="stylesheet" href="node_modules\bootstrap\dist\css\bootstrap.min.css">
     <link rel="stylesheet" href="src/core/style.min.css">
+    <link rel="stylesheet" href="node_modules\bootstrap\dist\css\bootstrap.min.css">
 </head>
 <body>
 <header>
