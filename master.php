21aa4005cee0fd5931e7fd06852402c75d5dc65e
adrianmcg85 <adrian.m.mcg@gmail.com>
2019-11-03 15:45:58 +0000

    added padding to a in nav

--- master.php
+++ master.php
@@ -6,8 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Master Estate Agent</title>
-    <link rel="stylesheet" href="node_modules\bootstrap\dist\css\bootstrap.min.css">
     <link rel="stylesheet" href="src/core/style.min.css">
+    <link rel="stylesheet" href="node_modules\bootstrap\dist\css\bootstrap.min.css">
+
 </head>
 
 <body>
