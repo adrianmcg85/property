aebf6061ba87c44db5f10fbb12fcc41844b31665
adrianmcg85 <adrian.m.mcg@gmail.com>
2019-11-03 15:15:48 +0000

    added top leve navigation

--- strip/navigation.php
+++ strip/navigation.php
@@ -5,7 +5,22 @@
     </button>
     <div class="collapse navbar-collapse" id="targetNav">
         <li class="nav-item active">
-            <a class="nav-link" href="#">Home</a>
+            <a class="nav-link" href="#">Buy</a>
+        </li>
+        <li class="nav-item active">
+            <a class="nav-link" href="#">Sell</a>
+        </li>
+        <li class="nav-item active">
+            <a class="nav-link" href="#">Rent</a>
+        </li>
+        <li class="nav-item active">
+            <a class="nav-link" href="#">Landlord</a>
+        </li>
+        <li class="nav-item active">
+            <a class="nav-link" href="#">Services</a>
+        </li>
+        <li class="nav-item active">
+            <a class="nav-link" href="#">Branches</a>
         </li>
         </ul>
     </div>
