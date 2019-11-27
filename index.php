5630988676b04b3d6560abcfb7aea15b1dda08f2
adrianmcg85 <adrian.m.mcg@gmail.com>
2019-11-27 16:14:17 +0000

    responsiveness added

--- index.php
+++ index.php
@@ -3,12 +3,10 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=false;">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Master Estate Agent</title>
     <link rel="stylesheet" href="src/core/style.min.css">
-
-
 </head>
 
 <body>
@@ -16,9 +14,9 @@
         <?php include'strip/navigation.php' ?>
 
     </header>
-    <script src="node_modules\jquery\dist\jquery.min.js"></script>
-    <script src="node_modules\bootstrap\dist\js\bootstrap.min.js"></script>
-    <script src="src\core\script.min.js"></script>
+    <script type="text/javascript" src="node_modules\jquery\dist\jquery.min.js"></script>
+    <script type="text/javascript" src="node_modules\bootstrap\dist\js\bootstrap.min.js"></script>
+    <!-- <script src="src\core\script.min.js"></script> -->
 </body>
 
 </html>
--- strip/navigation.php
+++ strip/navigation.php
@@ -1,13 +1,10 @@
-<div class="container">
+<div id="desktop_nav" class="container">
     <div class="row ">
         <div class="navbar navbar-expand-lg navbar-light">
             <div class="logo">
                 <a href="#" class="navbar-brand">Brand</a>
             </div>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#targetNav">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <nav class="collapse navbar-collapse" id="targetNav">
+            <nav class="navbar-collapse" >
                 <ul>
                     <li class="nav-item active">
                         <a class="nav-link" href="#">Buy</a>
@@ -57,3 +54,28 @@
 
     </div>
 </div>
+
+<nav id="responsive_nav" class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Brand</a>
+  <div class="ml-auto">
+  <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  </div>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Buy </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Sell</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Rent</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Landlord</a>
+      </li>
+    </ul>
+  </div>
+</nav>
