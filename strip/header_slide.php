7028be94bfe6300eb39eec3b4e266ed988b67d46
adrianmcg85 <adrian.m.mcg@gmail.com>
2019-11-27 16:37:42 +0000

    Pane content added

--- strip/header_slide.php
+++ strip/header_slide.php
@@ -2,3 +2,16 @@
     <!-- Use jpg image for this image to keep quality -->
     <img src="src\img\header_slide.jpg" alt="cat">
 </div>
+<div class="header_content">
+    <div class="pane">
+        <h2>Welcome</h2>
+        <h3>Takimata ipsum tempor amet et erat at.</h3>
+        <div class="btn_group">
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+        </div>
+        <h3>Rating</h3>
+        <p>Unto and misery than him gathered.</p>
+
+    </div>
+</div>
