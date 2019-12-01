bfbfbc648efe893a56cd354e4aaba028a77bafd7
adrianmcg85 <adrian.m.mcg@gmail.com>
2019-12-01 21:11:00 +0000

    header slide changes

--- strip/header_slide.php
+++ strip/header_slide.php
@@ -1,15 +1,15 @@
-<div class="header_image">
-    <!-- Use jpg image for this image to keep quality -->
-    <img src="src\img\header_slide.jpg" alt="cat">
-</div>
 <div class="header_content">
+    <div class="header_image">
+        <!-- Use jpg image for this image to keep quality -->
+        <img src="src\img\header_slide.jpg" alt="cat">
+    </div>
     <div class="pane">
         <h2>Welcome</h2>
-        <h3>Takimata ipsum tempor amet et erat at.</h3>
+        <h3>Takimata ipsum tempor amet et.</h3>
         <div class="btn_group">
-            <a href="#">Link 1</a>
-            <a href="#">Link 2</a>
-        </div>
+            <a href="#"><span>Link 1</span></a>
+            <a href="#"><span>Link 2</span></a>
+                </div>
         <h3>Rating</h3>
         <p>Unto and misery than him gathered.</p>
 
