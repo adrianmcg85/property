d9c94565431a2fb6216e096bb9e9e29317671e06
adrianmcg85 <adrian.m.mcg@gmail.com>
2019-12-04 15:42:58 +0000

    initial price mobile field styling

--- strip/property_search.php
+++ strip/property_search.php
@@ -10,29 +10,35 @@
         </div>
         <div class="location form_group">
             <input type="text" name="location">
-            <select name="radius">
-                <option value="0.5">0.5 Miles</option>
-                <option value="1">1 Miles</option>
-                <option value="2">2 Miles</option>
-                <option value="5">5 Miles</option>
-                <option value="10">10 Miles</option>
-            </select>
+            <div class="field_select">
+                <select name="radius">
+                    <option value="0.5">0.5 Miles</option>
+                    <option value="1">1 Miles</option>
+                    <option value="2">2 Miles</option>
+                    <option value="5">5 Miles</option>
+                    <option value="10">10 Miles</option>
+                </select>
+            </div>
         </div>
         <div class="price form_group">
-            <select>
-                <option value="100000">£100,000</option>
-                <option value="500000">£500,000</option>
-                <option value="1000000">£1000,000</option>
-                <option value="1500000">£1500,000</option>
-                <option value="2000000">£2000000</option>
-            </select>
-            <select>
-                <option value="100000">£100,000</option>
-                <option value="500000">£500,000</option>
-                <option value="1000000">£1000,000</option>
-                <option value="1500000">£1500,000</option>
-                <option value="2000000">£2000000</option>
-            </select>
+            <div class="field_select">
+                <select>
+                    <option value="100000">£100,000</option>
+                    <option value="500000">£500,000</option>
+                    <option value="1000000">£1000,000</option>
+                    <option value="1500000">£1500,000</option>
+                    <option value="2000000">£2000000</option>
+                </select>
+            </div>
+            <div class="field_select">
+                <select>
+                    <option value="100000">£100,000</option>
+                    <option value="500000">£500,000</option>
+                    <option value="1000000">£1000,000</option>
+                    <option value="1500000">£1500,000</option>
+                    <option value="2000000">£2000000</option>
+                </select>
+            </div>
         </div>
         <button type="submit form_group">Search</button>
     </form>
