cbb839fed5790f468569ba3e4629093ae6b47032
adrianmcg85 <adrian.m.mcg@gmail.com>
2019-12-09 21:41:06 +0000

    removed gf and jnp grunt instructions for now

--- gruntfile.js
+++ gruntfile.js
@@ -4,59 +4,17 @@ module.exports = function (grunt) {
     watch: {
       sass: {
         files: ['src/core/scss/*.scss',
-        //'src/gf/scss/*.scss', 'src/jnp/scss/*.scss', 
-       // 'src/core/js/*.js', 
-        //'src/gf/js/*.js', 'src/jnp/js/*.js'
       ],
         tasks: ['sass', 'cssmin']
       },
-      // uglify:{
-      //   files: ['src/core/*.js'
-      //   //,'src/gf/*.js','src/jnp/*.js'
-      // ],
-      //   tasks: ['concat', 'uglify']
-      // }
     },
-   // concat:{
-      // css_gf:{
-      //   src: [
-      //   'src/gf/style.css',
-      //   'src/core/style.css'],
-      // dest: 'src/gf/style.css'},
-      // csss_jnp:{
-      //   src: [
-      //   'src/jnp/style.css',
-      //   'src/core/style.css'],
-      // dest: 'src/jnp/style.css'},
-      // js_core:{
-      //   src:['src/core/js/*.js'],
-      //   dest:'src/core/script.js'
-      // }
-      // js_gf:{
-      //   src:['src/core/js/*.js','src/gf/js/*.js'],
-      //   dest:'src/gf/script.js'
-      // },
-      // js_jnp:{
-      //   src:['src/core/js/*.js','src/jnp/js/*.js'],
-      //   dest:'src/jnp/script.js'
-      // }
-
-   // },
     sass: {
       build:{
          files: [
           {
           src: 'src/core/scss/style.scss',
          dest: 'src/core/style.css'
-          } // ,
-        // {
-        //   src: 'src/gf/scss/style.scss',
-        //   dest: 'src/gf/style.css'
-        // },
-        // {
-        //   src: 'src/jnp/scss/style.scss',
-        //   dest: 'src/jnp/style.css'
-        // }
+          } 
       ]
       }
     },
@@ -65,30 +23,15 @@ module.exports = function (grunt) {
         src:'src/core/style.css',
         dest:"src/core/style.min.css"
       },
-      target_gf:{
-        src:'src/gf/style.css',
-        dest:"src/gf/style.min.css"
-      },     target_jnp:{
-        src:'src/jnp/style.css',
-        dest:"src/jnp/style.min.css"
-      }
     },
-    // uglify:{
-    //   target:{
-    //     files:[{
-    //       src: "src/core/script.js",
-    //       dest: "src/core/script.min.js" 
-    //     }],
-    //     files_gf:[{
-    //       src: "src/gf/script.js",
-    //       dest: "src/gf/script.min.js" 
-    //     }],
-    //     files_jnp:[{
-    //       src: "src/core/script.js",
-    //       dest: "src/core/script.min.js" 
-    //     }]
-    //   }
-    // }
+    uglify:{
+      target:{
+        files:[{
+          src: "src/core/script.js",
+          dest: "src/core/script.min.js" 
+        }]
+      }
+    }
 
   });
   grunt.loadNpmTasks('grunt-contrib-watch');
