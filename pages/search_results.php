989deb9b347a49025101d8c03b1ea0d12cc484da
adrianmcg85 <adrian.m.mcg@gmail.com>
2019-12-09 21:38:07 +0000

    an initial results page created

---
+++ pages/search_results.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=false;">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Master Estate Agent</title>
+    <link rel="stylesheet" href="../src/core/style.min.css">
+</head>
+
+<body>
+    <header>
+        <?php include'../strip/navigation.php' ?>
+
+    </header>
+    <section>
+    <?php include"../strip/property_search/property_search.php"?>
+    </section>
+    <script type="text/javascript" src="node_modules\jquery\dist\jquery.min.js"></script>
+    <script type="text/javascript" src="node_modules\bootstrap\dist\js\bootstrap.min.js"></script>
+    <!-- <script src="src\core\script.min.js"></script> -->
+</body>
+
+</html>
