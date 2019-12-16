b063e14f56fa19700a6b0113b5f014b06f13b3f7
adrianmcg85 <adrian.m.mcg@gmail.com>
2019-12-16 21:56:49 +0000

    main text content styled

--- strip/home/page_content.php
+++ strip/home/page_content.php
@@ -1,17 +1,53 @@
-<div class="container">
-<div class="left">
-<div class="row">
-<h1>Header Text</h1>
-</div>
-<div class="row">
-<div class="col_left">
-Justo invidunt sadipscing erat rebum et, sadipscing duo clita nonumy nonumy, tempor voluptua sed aliquyam magna et erat aliquyam et, stet kasd amet vero elitr eos ea magna. Gubergren nonumy diam duo tempor amet eos, erat no sadipscing eirmod ipsum at elitr at gubergren. Lorem clita tempor ea dolor ipsum.
-</div>
-<div class="col_right">
-Childe to this and loved like, earth  and ere nor girls a. Will bacchanals more formed vexed stalked to, sacred loved shun and congealed his who nor, from venerable sacred old will before degree. One uncouth into from that open things, harolds her their other take awake. Goodly charms.
-</div>
-</div>
-</div>
-    <div class="right">
+<div class="main_content">
+    <div class="container">
+        <div class="left">
+            <div class="row widget_header">
+                <h1>Diam stet dolor et kasd dolore, amet amet et dolor gubergren.</h1>
+            </div>
+            <div class="row widget_text">
+                <div class="col_left">
+                    <p>
+                        Justo invidunt sadipscing erat rebum et, sadipscing duo clita nonumy nonumy, tempor voluptua sed
+                        aliquyam magna et erat aliquyam et, stet kasd amet vero elitr eos ea magna. Gubergren nonumy
+                        diam duo
+                        tempor amet eos, erat no sadipscing eirmod ipsum at elitr at gubergren. Lorem clita tempor ea
+                        dolor
+                        ipsum.
+                    </p>
+                    <p>
+                        Justo invidunt sadipscing erat rebum et, sadipscing duo clita nonumy nonumy, tempor voluptua sed
+                        aliquyam magna et erat aliquyam et, stet kasd amet vero elitr eos ea magna. Gubergren nonumy
+                        diam duo
+                        tempor amet eos, erat no sadipscing eirmod ipsum at elitr at gubergren. Lorem clita tempor ea
+                        dolor
+                        ipsum.
+                    </p>
+                </div>
+                <div class="col_right">
+                    <p>
+                        Childe to this and loved like, earth and ere nor girls a. Will bacchanals more formed vexed
+                        stalked to,
+                        sacred loved shun and congealed his who nor, from venerable sacred old will before degree. One
+                        uncouth
+                        into from that open things, harolds her their other take awake. Goodly charms.
+                    </p>
+                    <p>
+                        Childe to this and loved like, earth and ere nor girls a. Will bacchanals more formed vexed
+                        stalked to,
+                        sacred loved shun and congealed his who nor, from venerable sacred old will before degree. One
+                        uncouth
+                        into from that open things, harolds her their other take awake. Goodly charms.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="right">
+            <img src="src/img/header_slide.jpg" alt="">
+            <div class="title">
+            <strong>Contact Us</strong>
+
+            </div>
+            <p>At rebum at justo accusam voluptua. No.</p>
+        </div>
     </div>
 </div>
