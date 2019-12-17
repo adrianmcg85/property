cb1bf29860a0fcfeb12a5c1a81377ad8fd9bb90e
adrianmcg85 <adrian.m.mcg@gmail.com>
2019-12-17 08:19:20 +0000

    image widget styling

--- strip/image_widget.php
+++ strip/image_widget.php
@@ -1,1 +1,27 @@
-test
+<div class="image_widget">
+<div class="container">
+    <div class="pre_text">
+    <h2>Text</h2>
+    </div>
+    <div class="row">
+    <div class="widget_image">
+        <a href="#">
+            <img src="src/img/header_slide.jpg" alt="">
+            <p class="title"><strong>title</strong></p>
+        </a>
+    </div>
+    <div class="widget_image">
+        <a href="#">
+            <img src="src/img/header_slide.jpg" alt="">
+            <p class="title"><strong>title</strong></p>
+        </a>
+    </div>
+    <div class="widget_image">
+        <a href="#">
+            <img src="src/img/header_slide.jpg" alt="">
+            <p class="title"><strong>title</strong></p>
+        </a>
+    </div>
+    </div>
+</div>
+</div>
