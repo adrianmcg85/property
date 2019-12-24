6698d4da619198b8ea3ab1c7b0c92d2fe1edadc9
adrianmcg85 <adrian.m.mcg@gmail.com>
2019-12-24 20:02:35 +0000

    styled search results in mobile

---
+++ strip/property_search/search_results_grid.php
@@ -0,0 +1,79 @@
+<div class="container">
+    <div class="search_results results_grid">
+        <div class="property">
+            <div class="img_section">
+                <div class="main_img">
+                    <a href="#">
+                        <img src="..\src\img\cat.png" alt="">
+                    </a>
+                </div>
+                <div class="additional_img">
+                    <div class="img">
+                        <img src="..\src\img\cat.png" alt="">
+                    </div>
+                    <div class="img">
+                        <img src="..\src\img\cat.png" alt="">
+                    </div>
+                    <div class="img">
+                        <img src="..\src\img\cat.png" alt="">
+                    </div>
+                    <div class="img">
+                        <img src="..\src\img\cat.png" alt="">
+                    </div>
+                </div>
+                <div class="img_button">
+                        <a class="btn" href="#">Images</a>
+                    </div>
+            </div>
+            <div class="info_section">
+                <div class="info_header">
+                    <div class="info_header_left">
+                        <p>X Bedroom property</p>
+                        <h2>Guide Price</h2>
+                        <h3>£100,000</h3>
+                    </div>
+                    <div class="info_header_right">
+                        <span>x</span>
+                        <span>x</span>
+                        <span>x</span>
+                    </div>
+
+                </div>
+                <div class="info_address">
+                    <p>Street, Town, xx1 1xx</p>
+                </div>
+                <div class="info_description">
+                    <p>
+                        Voluptua kasd diam erat kasd eos, sit at amet sanctus et lorem accusam no et takimata. Lorem
+                        takimata magna diam sadipscing at kasd. Diam et et sea elitr eos no.
+                    </p>
+
+                </div>
+                <div class="info_features">
+                    <span>Features</span>
+                    <ul>
+                        <li>Lorem sit eirmod et magna.</li>
+                        <li>Lorem sit eirmod et magna.</li>
+                        <li>Lorem sit eirmod et magna.</li>
+                        <li>Lorem sit eirmod et magna.</li>
+                    </ul>
+
+                </div>
+                <div class="info_buttons">
+                    <a class="btn" href="#">Button</a>
+                    <a class="btn" href="#">Button</a>
+                    <a class="btn" href="#">Button</a>
+                </div>
+
+            </div>
+            <div class="footer_section">
+                <span class="contact">
+                    Cal: 01234 567890
+                </span>
+                <span class="ref">
+                    Ref: 12341234
+                </span>
+            </div>
+        </div>
+    </div>
+</div>
