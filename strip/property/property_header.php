41838964d004ea352c3a5347f541ef555dd723b9
adrianmcg85 <adrian.m.mcg@gmail.com>
2019-12-25 23:46:50 +0000

    added content + initial styling to property header

--- strip/property/property_header.php
+++ strip/property/property_header.php
@@ -1,1 +1,21 @@
-<h1>test</h1>
+<div class="container">
+    
+<div class="property_header">
+    <div class="header_address">
+        <span class="details_blurb">
+        bedroom semi detached house for sale
+        </span>
+        <span class="address">
+            x street road, town, TA1
+        </span>
+    </div>
+    <div class="header_price">
+        <p class="price_premessage">Asking Price</p>
+        <p class="price">£100,000</p>
+    </div>
+    <div class="header_buttons">
+        <a href="#" class="back_search">Back to search</a>
+        <a href="#" class="btn">Book a viewing</a>
+    </div>
+</div>
+</div>
