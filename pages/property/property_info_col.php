26b5ac37a21184329a70d81106afb2e3e7e4822d
adrianmcg85 <adrian.m.mcg@gmail.com>
2019-12-27 15:58:17 +0000

    content and style fo content and sub nav

--- pages/property/property_info_col.php
+++ pages/property/property_info_col.php
@@ -2,4 +2,7 @@
     <div class="property_img">
         <?php include "./../strip/property/property_carousel.php"?>
     </div>
+    <div class="">
+        <?php include "./../strip/property/property_content.php"?>
+    </div>
 </div>
