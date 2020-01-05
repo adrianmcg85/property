5edd87a940dcd3508f2a5ffb66f6c32c2e51b5dd
adrianmcg85 <adrian.m.mcg@gmail.com>
2020-01-05 21:45:41 +0000

    added a footer navigation for desktop only

---
+++ strip/footer.php
@@ -0,0 +1,52 @@
+<footer>
+    <div class="container">
+    <div class="footer_col">
+        <span class="footer_col_header">Title</span>
+        <ul class="footer_sub_list">
+            <li class="list_item">List Item</li>
+            <li class="list_item">List Item</li>
+            <li class="list_item">List Item</li>
+        </ul>
+    </div>
+    <div class="footer_col">
+        <span class="footer_col_header">Title</span>
+        <ul class="footer_sub_list">
+            <li class="list_item">List Item</li>
+            <li class="list_item">List Item</li>
+            <li class="list_item">List Item</li>
+        </ul>
+    </div>
+    <div class="footer_col">
+        <span class="footer_col_header">Title</span>
+        <ul class="footer_sub_list">
+            <li class="list_item">List Item</li>
+            <li class="list_item">List Item</li>
+            <li class="list_item">List Item</li>
+        </ul>
+    </div>
+    <div class="footer_col">
+        <span class="footer_col_header">Title</span>
+        <ul class="footer_sub_list">
+            <li class="list_item">List Item</li>
+            <li class="list_item">List Item</li>
+            <li class="list_item">List Item</li>
+        </ul>
+    </div>
+    <div class="footer_col">
+        <span class="footer_col_header">Title</span>
+        <ul class="footer_sub_list">
+            <li class="list_item">List Item</li>
+            <li class="list_item">List Item</li>
+            <li class="list_item">List Item</li>
+        </ul>
+    </div>
+    <div class="footer_col">
+        <span class="footer_col_header">Title</span>
+        <ul class="footer_sub_list">
+            <li class="list_item">List Item</li>
+            <li class="list_item">List Item</li>
+            <li class="list_item">List Item</li>
+        </ul>
+    </div>
+    </div>
+</footer>
