4e7bbc994612696537def90bb92c0cf82da7032d
adrianmcg85 <adrian.m.mcg@gmail.com>
2020-01-05 12:05:20 +0000

    added property right column

---
+++ pages/property/property_header2_col.php
@@ -0,0 +1,19 @@
+<div class="header2_col">
+    <div class="key_features">
+        <h3>Key Featutes</h3>
+        <div class="feature_list">
+            <div class="feature">
+                Feature
+            </div>
+            <div class="feature">
+            Feature
+            </div>
+            <div class="feature">
+            Feature
+            </div>
+            <div class="feature">
+            Feature
+            </div>
+        </div>
+    </div>
+</div>
