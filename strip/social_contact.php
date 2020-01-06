3f512170a517cb61f22e235edc5fc2ecb54407a6
adrianmcg85 <adrian.m.mcg@gmail.com>
2020-01-06 22:12:21 +0000

    futher styling for social widet for jnp

--- strip/social_contact.php
+++ strip/social_contact.php
@@ -1,5 +1,5 @@
-<div class="container">
-    <div class="social_contact">
+<div class="social_contact">
+    <div class="container contact_wrapper">
         <div class="social_contacct_block">Sea sadipscing sanctus.</div>
         <div class="social_contacct_block">Sea sadipscing sanctus.</div>
         <div class="social_contacct_block">Sea sadipscing sanctus.</div>
