8d17371b34cf36ac2d4875f9262b3b5a11efb7d2
adrianmcg85 <adrian.m.mcg@gmail.com>
2020-01-08 10:49:40 +0000

    altered header slide class names to BEM

--- strip/header_slide.php
+++ strip/header_slide.php
@@ -1,9 +1,9 @@
 <div class="header_content">
-    <div class="header_image">
+    <div class="header_content__image header_image">
         <!-- Use jpg image for this image to keep quality -->
         <img src="src\img\header_slide.jpg" alt="cat">
     </div>
-    <div class="pane">
+    <div class="header_content__pane">
         <h2>Welcome</h2>
         <h3>Takimata ipsum tempor amet et.</h3>
         <div class="btn_group">
