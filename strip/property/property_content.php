3f544b6f8d147f75e9445b36aa34751eb0235d48
adrianmcg85 <adrian.m.mcg@gmail.com>
2020-01-08 15:19:17 +0000

    altered property content to use bem format

--- strip/property/property_content.php
+++ strip/property/property_content.php
@@ -21,26 +21,26 @@
         Sit et est tempor sit sed sed amet dolore et sadipscing, et et lorem clita voluptua nonumy dolor amet, voluptua takimata gubergren erat sed dolores dolor sea, no lorem tempor accusam vero vero. Sadipscing sed voluptua magna takimata invidunt erat. Diam ut clita dolore diam amet kasd, diam diam diam et eirmod dolore. Ipsum ea diam magna gubergren. Ea sanctus kasd dolor kasd vero rebum, et sea sanctus sadipscing amet vero accusam eirmod clita justo. Sed aliquyam stet clita sed invidunt dolor kasd rebum ea. Consetetur eirmod erat vero dolor ut, est sit ut voluptua tempor diam stet stet, erat ea amet vero erat takimata takimata sit stet. Ipsum eos diam sed no sadipscing. Duo nonumy voluptua accusam tempor ut, duo sed elitr et accusam justo amet est amet labore. Magna dolor dolores labore accusam et aliquyam, est sed sanctus erat kasd et diam aliquyam, ipsum et sed justo eirmod sit. Gubergren duo et kasd sit erat. Et sed nonumy diam ipsum et diam kasd. Eos et lorem eirmod takimata dolores. Nonumy sit lorem dolor eirmod eos sadipscing vero, dolor takimata dolores eirmod tempor duo, dolor ea vero ea sed gubergren sed lorem, no diam invidunt accusam dolore justo sed. At accusam rebum diam amet justo ut amet invidunt amet. Ipsum justo dolor sed diam rebum et ea at, stet sadipscing lorem sed invidunt et dolor sit dolore sit, justo at sea justo dolor dolor, sea tempor elitr amet erat labore sit dolor sed. Eos lorem lorem sed et dolor ea dolor kasd. Rebum diam lorem dolor justo sanctus clita labore, sed est vero sit ut sed, consetetur no et sit est amet aliquyam gubergren. Et sadipscing lorem gubergren no voluptua accusam et tempor, tempor sea nonumy clita nonumy eos stet. Aliquyam diam nonumy kasd stet labore et elitr eos.
     </p>
 
-    <div class="table">
-        <div class="table_row">
-            <div class="room">Reception Room</div>
-            <div class="dimensions">5.30m x 3.56m (17'5" x 11'8")</div>
-            <div class="popup">+</div>
+    <div class="property_table">
+        <div class="property_table__row">
+            <div class="property_item__room">Reception Room</div>
+            <div class="property_item__dimensions">5.30m x 3.56m (17'5" x 11'8")</div>
+            <div class="property_item__popup">+</div>
         </div>
-        <div class="table_row">
-            <div class="room">Dining Room</div>
-            <div class="dimensions">5.30m x 3.56m (17'5" x 11'8")</div>
-            <div class="popup">+</div>
+        <div class="property_table__row">
+            <div class="property_item__room">Dining Room</div>
+            <div class="property_item__dimensions">5.30m x 3.56m (17'5" x 11'8")</div>
+            <div class="property_item__popup">+</div>
         </div>
-        <div class="table_row">
-            <div class="room">Bedroom Room</div>
-            <div class="dimensions">5.30m x 3.56m (17'5" x 11'8")</div>
-            <div class="popup">+</div>
+        <div class="property_table__row">
+            <div class="property_item__room">Bedroom Room</div>
+            <div class="property_item__dimensions">5.30m x 3.56m (17'5" x 11'8")</div>
+            <div class="property_item__popup">+</div>
         </div>
-        <div class="table_row">
-            <div class="room">Kitchen</div>
-            <div class="dimensions">5.30m x 3.56m (17'5" x 11'8")</div>
-            <div class="popup">+</div>
+        <div class="property_table__row">
+            <div class="property_item__room">Kitchen</div>
+            <div class="property_item__dimensions">5.30m x 3.56m (17'5" x 11'8")</div>
+            <div class="property_item__popup">+</div>
         </div>
     </div>
 </div>
