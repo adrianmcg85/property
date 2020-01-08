c40acb06d431cbd1e5a569649b06c9ba98b4ff45
adrianmcg85 <adrian.m.mcg@gmail.com>
2020-01-08 11:35:47 +0000

    changed image_widget to be more bem

--- strip/image_widget.php
+++ strip/image_widget.php
@@ -1,27 +1,27 @@
 <div class="image_widget">
-<div class="container">
-    <div class="pre_text">
-    <h2>Text</h2>
+    <div class="container">
+        <div class="widget__pre_text">
+            <h2>Text</h2>
+        </div>
+        <div class="row">
+            <div class="widget_image">
+                <a href="#">
+                    <img src="src/img/header_slide.jpg" alt="">
+                    <p class="widget_image__title"><strong>title</strong></p>
+                </a>
+            </div>
+            <div class="widget_image">
+                <a href="#">
+                    <img src="src/img/header_slide.jpg" alt="">
+                    <p class="widget_image__title"><strong>title</strong></p>
+                </a>
+            </div>
+            <div class="widget_image">
+                <a href="#">
+                    <img src="src/img/header_slide.jpg" alt="">
+                    <p class="widget_image__title"><strong>title</strong></p>
+                </a>
+            </div>
+        </div>
     </div>
-    <div class="row">
-    <div class="widget_image">
-        <a href="#">
-            <img src="src/img/header_slide.jpg" alt="">
-            <p class="title"><strong>title</strong></p>
-        </a>
-    </div>
-    <div class="widget_image">
-        <a href="#">
-            <img src="src/img/header_slide.jpg" alt="">
-            <p class="title"><strong>title</strong></p>
-        </a>
-    </div>
-    <div class="widget_image">
-        <a href="#">
-            <img src="src/img/header_slide.jpg" alt="">
-            <p class="title"><strong>title</strong></p>
-        </a>
-    </div>
-    </div>
-</div>
 </div>
