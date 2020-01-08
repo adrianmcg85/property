5ceeae15261ae593695f13d2f39153d658edcfac
adrianmcg85 <adrian.m.mcg@gmail.com>
2020-01-08 11:56:37 +0000

    class of social_contact changed  to bem

--- strip/social_contact.php
+++ strip/social_contact.php
@@ -1,7 +1,7 @@
 <div class="social_contact">
     <div class="container contact_wrapper">
-        <div class="social_contacct_block">Sea sadipscing sanctus.</div>
-        <div class="social_contacct_block">Sea sadipscing sanctus.</div>
-        <div class="social_contacct_block">Sea sadipscing sanctus.</div>
+        <div class="social_contacct__block">Sea sadipscing sanctus.</div>
+        <div class="social_contacct__block">Sea sadipscing sanctus.</div>
+        <div class="social_contacct__block">Sea sadipscing sanctus.</div>
     </div>
 </div>
