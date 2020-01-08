0cc6052c8f03323c7d75144f8c90d7754700851e
adrianmcg85 <adrian.m.mcg@gmail.com>
2020-01-08 15:26:29 +0000

    altered property feature column to use bem format

--- pages/property/property_header2_col.php
+++ pages/property/property_header2_col.php
@@ -1,17 +1,17 @@
 <div class="header2_col">
     <div class="key_features">
         <h3>Key Featutes</h3>
-        <div class="feature_list">
-            <div class="feature">
+        <div class="key_features__list">
+            <div class="feature_list__feature">
                 Feature
             </div>
-            <div class="feature">
+            <div class="feature_list__feature">
             Feature
             </div>
-            <div class="feature">
+            <div class="feature_list__feature">
             Feature
             </div>
-            <div class="feature">
+            <div class="feature_list__feature">
             Feature
             </div>
         </div>
