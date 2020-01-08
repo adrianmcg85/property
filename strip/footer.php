40cab1f6f2421a6198066bd9206740c38f9f492f
adrianmcg85 <adrian.m.mcg@gmail.com>
2020-01-08 12:07:26 +0000

    footer class altered to fit bem

--- strip/footer.php
+++ strip/footer.php
@@ -1,51 +1,51 @@
 <footer>
     <div class="container">
     <div class="footer_col">
-        <span class="footer_col_header">Title</span>
-        <ul class="footer_sub_list">
-            <li class="list_item">List Item</li>
-            <li class="list_item">List Item</li>
-            <li class="list_item">List Item</li>
+        <span class="footer_col__header">Title</span>
+        <ul class="footer__sub_list">
+            <li class="sub_list__item">List Item</li>
+            <li class="sub_list__item">List Item</li>
+            <li class="sub_list__item">List Item</li>
         </ul>
     </div>
     <div class="footer_col">
-        <span class="footer_col_header">Title</span>
-        <ul class="footer_sub_list">
-            <li class="list_item">List Item</li>
-            <li class="list_item">List Item</li>
-            <li class="list_item">List Item</li>
+        <span class="footer_col__header">Title</span>
+        <ul class="footer__sub_list">
+            <li class="sub_list__item">List Item</li>
+            <li class="sub_list__item">List Item</li>
+            <li class="sub_list__item">List Item</li>
         </ul>
     </div>
     <div class="footer_col">
-        <span class="footer_col_header">Title</span>
-        <ul class="footer_sub_list">
-            <li class="list_item">List Item</li>
-            <li class="list_item">List Item</li>
-            <li class="list_item">List Item</li>
+        <span class="footer_col__header">Title</span>
+        <ul class="footer__sub_list">
+            <li class="sub_list__item">List Item</li>
+            <li class="sub_list__item">List Item</li>
+            <li class="sub_list__item">List Item</li>
         </ul>
     </div>
     <div class="footer_col">
-        <span class="footer_col_header">Title</span>
-        <ul class="footer_sub_list">
-            <li class="list_item">List Item</li>
-            <li class="list_item">List Item</li>
-            <li class="list_item">List Item</li>
+        <span class="footer_col__header">Title</span>
+        <ul class="footer__sub_list">
+            <li class="sub_list__item">List Item</li>
+            <li class="sub_list__item">List Item</li>
+            <li class="sub_list__item">List Item</li>
         </ul>
     </div>
     <div class="footer_col">
-        <span class="footer_col_header">Title</span>
-        <ul class="footer_sub_list">
-            <li class="list_item">List Item</li>
-            <li class="list_item">List Item</li>
-            <li class="list_item">List Item</li>
+        <span class="footer_col__header">Title</span>
+        <ul class="footer__sub_list">
+            <li class="sub_list__item">List Item</li>
+            <li class="sub_list__item">List Item</li>
+            <li class="sub_list__item">List Item</li>
         </ul>
     </div>
     <div class="footer_col">
-        <span class="footer_col_header">Title</span>
-        <ul class="footer_sub_list">
-            <li class="list_item">List Item</li>
-            <li class="list_item">List Item</li>
-            <li class="list_item">List Item</li>
+        <span class="footer_col__header">Title</span>
+        <ul class="footer__sub_list">
+            <li class="sub_list__item">List Item</li>
+            <li class="sub_list__item">List Item</li>
+            <li class="sub_list__item">List Item</li>
         </ul>
     </div>
     </div>
