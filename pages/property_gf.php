238911b66351beb926d758e26922d60d2be530b2
adrianmcg85 <adrian.m.mcg@gmail.com>
2020-01-11 23:54:36 +0000

    updated jquery and bootstrap to cdn for heroku

--- pages/property_gf.php
+++ pages/property_gf.php
@@ -30,8 +30,9 @@
     </section>
     <script type="module" src="..\node_modules\bootstrap\js\src\carousel.js"></script>
     <script type="module" src="..\node_modules\bootstrap\js\src\util.js"></script>
-    <script type="text/javascript" src="..\node_modules\jquery\dist\jquery.min.js"></script>
-    <script type="text/javascript" src="..\node_modules\bootstrap\dist\js\bootstrap.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
     <!-- <script src="src\core\script.min.js"></script> -->
     <script type="text/javascript" src="../src/core/js/property.js"></script>
 </body>
--- pages/search_results_grid.php
+++ pages/search_results_grid.php
@@ -24,8 +24,9 @@
     <?php include"../strip/property_search/search_results_header.php"?>
     <?php include"../strip/property_search/search_results_grid.php" ?>
     </section>
-    <script type="text/javascript" src="..\node_modules\jquery\dist\jquery.min.js"></script>
-    <script type="text/javascript" src="..\node_modules\bootstrap\dist\js\bootstrap.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
     <!-- <script src="src\core\script.min.js"></script> -->
 </body>
 
