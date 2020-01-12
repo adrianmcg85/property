c3a200431c4bc7cb1d837264d3a68a31b86091bd
adrianmcg85 <adrian.m.mcg@gmail.com>
2020-01-12 12:29:01 +0000

    removed p tags from ext library strip

--- strip/script_ext_library.php
+++ strip/script_ext_library.php
@@ -1,9 +1,7 @@
 <?php if($_SERVER['SERVER_NAME'] === 'localhost'):?>
 <?php if(substr(basename($_SERVER['SCRIPT_NAME']),0,5) === "index"):?>
-<p>index</p>
     <?php include 'strip/script_ext_library.php'?>
 <?php else: ?>
-<p>not index</p>
     <?php include '../strip/script_ext_library.php'?>
 <?php endif ?>
 <?php else: ?>
